c91d952589854a8f6f3d1bc0120e1a7973996c0c
Sithik <sithik@usistech.com>
2020-05-23 17:50:20 +0530

    New route
    /users/{id}

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('/users/{id}', function ($id) {
+    return App\Models\Users::findOrFail($id);
+});
